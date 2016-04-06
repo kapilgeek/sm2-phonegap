896f853df7bc235ffaa02dd836ed7542a852c176
SAMET <akul@charterresources.us>
2016-04-06 10:48:56 -0400

    latest update from kapil

--- addons/missingassignment/controllers/list.js
+++ addons/missingassignment/controllers/list.js
@@ -31,17 +31,14 @@ angular.module('mm.addons.missingassignment')
     $scope.missingassignmentcourse = [];
 
 
-    $scope.action = function(e, course) {
-        //alert('asdf');
-       // console.log(course);
+    $scope.action = function(e, course) {      
         $state.go('site.mm_course', {course: course});
         e.preventDefault();
         e.stopPropagation();
     };
     
     $scope.actionGrade = function(e, course) {
-        //alert('asdf');
-       // console.log(course);
+      
         $state.go('site.grades', {course: course});
         e.preventDefault();
         e.stopPropagation();
@@ -57,14 +54,12 @@ angular.module('mm.addons.missingassignment')
 
 
         return $mmaMissingassignment.getMissingassignment(true,unreadCount, mmaMissingassignmentListLimit).then(function(gotSummary) {
-            $scope.missingassignment = gotSummary;
-           // console.log(gotSummary);
+            $scope.missingassignment = gotSummary;          
             $scope.missingassignmentcourse = gotSummary.summary['courseData'];
-           // console.log(gotSummary.summary['courseData']);
-            //console.log(gotSummary.summary.courseData);
+         
             $scope.canLoadMore = false;
         }, function(error) {
-                   // alert('ocean three');
+                   
                     if (error) {
                         $mmUtil.showErrorModal(error);
                     } else {
@@ -73,50 +68,7 @@ angular.module('mm.addons.missingassignment')
                     $scope.canLoadMore = false;
         });
 
-        /*return $mmaMissingassignment.getUnreadMissingassignment(unreadCount, mmaMissingassignmentListLimit).then(function(unread) {
-            // Don't add the unread notifications to $scope.notifications yet. If there are no unread notifications
-            // that causes that the "There are no notifications" message is shown in pull to refresh.
-            unreadCount += unread.length;
-
-            if (unread.length < mmaMissingassignmentListLimit) {
-                // Limit not reached. Get read notifications until reach the limit.
-                var readLimit = mmaMissingassignmentListLimit - unread.length;
-                return $mmaMissingassignment.getReadMissingassignment(readCount, readLimit).then(function(read) {
-                    readCount += read.length;
-                    if (refresh) {
-                        $scope.missingassignment = unread.concat(read);
-                    } else {
-                        $scope.missingassignment = $scope.missingassignment.concat(unread).concat(read);
-                    }
-                    $scope.canLoadMore = read.length >= readLimit;
-                    $scope.summaryHTML = $sce.trustAsHtml($scope.missingassignment[0].summary);
-                }, function(error) {
-                    if (unread.length == 0) {
-                        if (error) {
-                            $mmUtil.showErrorModal(error);
-                        } else {
-                            $mmUtil.showErrorModal('mma.missingassignment.errorgetMissingassignment', true);
-                        }
-                        $scope.canLoadMore = false; // Set to false to prevent infinite calls with infinite-loading.
-                    }
-                });
-            } else {
-                if (refresh) {
-                    $scope.missingassignment = unread;
-                } else {
-                    $scope.missingassignment = $scope.missingassignment.concat(unread);
-                }
-                $scope.summaryHTML = $sce.trustAsHtml($scope.missingassignment[0].summary);
-                $scope.canLoadMore = true;
-            }
-        }, function(error) {
-            if (error) {
-                $mmUtil.showErrorModal(error);
-            } else {
-                $mmUtil.showErrorModal('mma.missingassignment.errorgetmissingassignment', true);
-            }
-            $scope.canLoadMore = false; // Set to false to prevent infinite calls with infinite-loading.
-        });*/
+       
     }
     fetchMissingassignment().finally(function() {
         $scope.missingassignmentLoaded = true;
